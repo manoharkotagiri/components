4d9cebd723274a6b5f12604899744c322afb5cb4
manoharkotagiri <manoharkotagiri@gmail.com>
2020-12-10 09:34:36 -0500

    initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+<
+src/index.js,f\9\f900edc44cb929c2267a16100a32851f6d5b477d
+A
+package-lock.json,f\a\fa288d1472d29beccb489a676f68739ad365fc47
+<
+package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
+D
+src/CommentDetail.js,c\c\cc844c7b16da602ea5c71f8702e6901ce9be8032
+A
+public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
+C
+src/ApprovalCard.js,6\a\6a9bcc098a524f9e11118b0f38340aec1e0880b0
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard> <CommentDetail author="Sam" timeAgo="Today at 4:45PM" content="Nice blog post!"
+                                          avatar={'https://source.unsplash.com/random'}/> </ApprovalCard>
+            <ApprovalCard> <CommentDetail author="Alex" timeAgo="Today at 2:00AM" content="React is the best"
+                                          avatar={'https://source.unsplash.com/random'}/> </ApprovalCard>
+            <ApprovalCard> <CommentDetail author="Jane" timeAgo="Yesterday at 5:45PM" content="I am learning React"
+                                          avatar={'https://source.unsplash.com/random'}/> </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
